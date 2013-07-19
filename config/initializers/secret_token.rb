f995d3b566dabe9de46aef945fa9a1d0c43b4981
kapysh <will@kapysh.com>
2013-07-18 23:48:26 -0700

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limeape::Application.config.secret_token = 'c8ad6b5bae17e096884596a5744d84f1152606a6e058019de8d2e18bec70f8fe4decb4ac0ba0105b9d0df0805866b6871645b9f54ffc985bdd730619b5ca27e6'
